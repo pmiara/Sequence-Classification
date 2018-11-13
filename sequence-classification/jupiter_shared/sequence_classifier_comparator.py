c8e26e44c4be4322ffed71e2fde3df548549ad2e
kasztanka <m.lipowska@onet.pl>
2018-11-13 18:38:17 +0100

    Add refactors from review

--- sequence-classification/jupiter_shared/sequence_classifier_comparator.py
+++ sequence-classification/jupiter_shared/sequence_classifier_comparator.py
@@ -3,26 +3,25 @@ from sklearn.model_selection import GridSearchCV, train_test_split
 
 
 class SequenceClassifierComparator:
-    def __init__(self, writer, reader, classifiers=None):
-        if classifiers is None:
-            classifiers = []
-        self.classifiers = classifiers
+    def __init__(self, writer, reader, classifier_triplets=None):
+        if classifier_triplets is None:
+            classifier_triplets = []
+        self.classifier_triplets = classifier_triplets
         self.writer = writer
         self.reader = reader
 
     def add_classifier(self, classifier, params=None, sequence_transformer=None):
         if params is None:
             params = {}
-        self.classifiers.append((classifier, params, sequence_transformer))
+        self.classifier_triplets.append((classifier, params, sequence_transformer))
 
-    def fit_predict(self, X, y, split_params=None, repetitions=3, cv=3):
+    def fit_predict(self, X, y, split_params=None, rounds=3, cv=3):
         if split_params is None:
             split_params = {}
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, **split_params)
-        for classifier, params, transformer in self.classifiers:
+        X_train, X_test, y_train, y_test = train_test_split(X, y, **split_params)
+        for classifier, params, transformer in self.classifier_triplets:
             X_train_transform, X_pred_transform = self.transform_data(X_train, X_test, transformer)
-            for i in range(repetitions):
+            for _ in range(rounds):
                 grid = GridSearchCV(classifier, params, cv=cv)
                 grid.fit(X_train_transform, y_train)
 
