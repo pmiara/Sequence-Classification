f8aadecc50bbe7271c221e8d3e98a872e8211912
kasztanka <m.lipowska@onet.pl>
2018-11-17 22:21:59 +0100

    Fix how_to_custom
    - add fit_transform to CustomTransformer
    - uncomment usage
    - pass writer and reader to comparator

--- sequence-classification/jupiter_shared/how_to_custom.py
+++ sequence-classification/jupiter_shared/how_to_custom.py
@@ -1,14 +1,18 @@
 from sequence_transformer import SequenceTransformer
 from sequence_classifier import SequenceClassifier
 from sequence_classifier_comparator import SequenceClassifierComparator
+from results_writer import ResultsWriter
+from results_reader import ResultsReader
 
 
-# example
 class CustomTransformer(SequenceTransformer):
     def __init__(self):
         SequenceTransformer.__init__(self)
 
     def transform(self, data):
+        return data
+
+    def fit_transform(self, data):
         return data
 
 
@@ -43,10 +47,12 @@ class CustomClassifier2(SequenceClassifier):
         return result
 
 
-# custom_transformer = CustomTransformer()
-# custom_classifier = CustomClassifier("custom1")
-# custom_classifier2 = CustomClassifier2("custom2")
-# comparator = SequenceClassifierComparator()
-# comparator.add_classifier(custom_classifier, custom_transformer)
-# comparator.add_classifier(custom_classifier2)
-# comparator.fit_predict([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
+if __name__ == '__main__':
+    custom_transformer = CustomTransformer()
+    custom_classifier = CustomClassifier('custom1')
+    custom_classifier2 = CustomClassifier2('custom2')
+    comparator = SequenceClassifierComparator(ResultsWriter(), ResultsReader())
+    comparator.add_classifier(custom_classifier, sequence_transformer=custom_transformer)
+    comparator.add_classifier(custom_classifier2)
+    comparator.fit_predict([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
+    comparator.plot_comparison()
