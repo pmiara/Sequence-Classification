6efd4b2d212a973835c67f805f0e6e2d4e235b35
grzegorz <grzegorz.szczepaniak18@gmail.com>
2018-11-17 13:52:57 +0100

    merging master into branch

--- sequence-classification/jupiter_shared/sequence_classifier_comparator.py
+++ sequence-classification/jupiter_shared/sequence_classifier_comparator.py
@@ -19,32 +19,6 @@ class SequenceClassifierComparator:
             params = {}
         self.classifier_triplets.append((classifier, params, sequence_transformer))
 
-    def fit_classifiers(self, X_train, y_train):
-        for classifier, transformer in self.classifiers:
-            if transformer is not None:
-                classifier.fit(transformer.transform(X_train), y_train)
-            else:
-                classifier.fit(X_train, y_train)
-
-    def predict_classifiers(self, X):
-        self.predictions = []
-        for classifier, transformer in self.classifiers:
-            if transformer is not None:
-                y_pred = classifier.predict(transformer.transform(X))
-            else:
-                y_pred = classifier.predict(X)
-            self.predictions.append((classifier.name, y_pred))
-        return self.predictions
-
-    def score_classifiers(self, X_test, y_test, results_writer):
-        for name, y_pred in self.predict_classifiers(X_test):
-            y_pred = [0 if i < 0.5 else 1 for i in y_pred]
-            accuracy = accuracy_score(y_test, y_pred)
-            matrix = confusion_matrix(y_test, y_pred)
-            params = {"warstwy": 2, "koza": 3, "cztery": 4}
-            results_writer.write_results(name,params,matrix, matrix)
-            self.scores.append((name, accuracy))
-        return self.scores
     def fit_predict(self, X, y, split_params=None, rounds=3, cv=3):
         if split_params is None:
             split_params = {}
@@ -78,7 +52,7 @@ class SequenceClassifierComparator:
         return X_train_transform, X_pred_transform
 
     def plot_comparison(self, results_reader):
-        classifier_names = [c[0].name for c in self.classifiers]
+        classifier_names = [c[0].name for c in self.classifier_triplets]
         results = results_reader.read_results(classifier_names)
         for name, values in results:
             print("--------------")
