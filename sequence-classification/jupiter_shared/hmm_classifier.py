45026d72a9abc690d728761020152259bd0ab313
berezowskij <jakub.berezowski@snpgroup.com>
2018-11-19 17:06:45 +0100

    Add basic HMM multinominal classifier

---
+++ sequence-classification/jupiter_shared/hmm_classifier.py
@@ -0,0 +1,36 @@
+from sequence_classifier import SequenceClassifier
+from sequence_transformer import SequenceTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+
+from seqlearn.hmm import MultinomialHMM
+
+class HMMClassifier(SequenceClassifier):
+    def __init__(self, name='HMM'):
+        super(HMMClassifier, self).__init__(name)
+        self.model = MultinomialHMM()
+
+    def fit(self, X_train, y_train):
+        self.model.fit(X_train, y_train, len(X_train))
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+    def get_transformer(self):
+        return HMMTransformer()
+
+
+class HMMTransformer(SequenceTransformer):
+    def __init__(self):
+        self.vectorizer = CountVectorizer()
+
+    def fit_transform(self, raw_data):
+        strings_from_list = [' '.join([str(x) for x in data]) for data in raw_data]
+        self.vectorizer.fit_transform(strings_from_list)
+        return self.vectorizer.fit_transform(strings_from_list).toarray()
+
+    def transform(self, raw_data):
+        strings_from_list = [' '.join([str(x) for x in data]) for data in raw_data]
+        return self.vectorizer.transform(strings_from_list).toarray()
+
+
+
