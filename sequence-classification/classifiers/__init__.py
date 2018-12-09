65c466f2583448f4c5e886016edc09ec869dfd1e
Piotr Miara <piotr@atopik.pl>
2018-12-09 16:49:00 +0100

    Add PatternBasedClassifier to 'asterisk import'

--- sequence-classification/classifiers/__init__.py
+++ sequence-classification/classifiers/__init__.py
@@ -1,7 +1,8 @@
 from .hmm_classifier import HMMClassifier
 from .knn_classifier import KNNClassifier
 from .neural_networks_classifier import NeuralNetworksClassifier
+from .pattern_based_classifier import PatternBasedClassifier
 from .svm_classifier import SVMClassifier
 
 
-__all__ = ['HMMClassifier', 'KNNClassifier', 'SVMClassifier', 'NeuralNetworksClassifier']
+__all__ = ['HMMClassifier', 'KNNClassifier', 'SVMClassifier', 'NeuralNetworksClassifier', 'PatternBasedClassifier']
