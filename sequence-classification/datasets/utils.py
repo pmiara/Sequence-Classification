2c31a647deceeba6388f00fafdcf4e0c22b30358
kasztanka <m.lipowska@onet.pl>
2019-01-02 17:59:18 +0100

    Standarize loading datasets
    - create DatasetLoader for each dataset
    - each dataset has its own folder with __init__ file containing
    class inheriting from DatasetLoader and having load_data method
    - load_data method returns Dataset
    - remove unnecessary files

--- sequence-classification/datasets/utils.py
+++ sequence-classification/datasets/utils.py
@@ -1,4 +1,7 @@
+import re
+import numpy as np
 from os import path
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 class DatasetLoader:
@@ -17,3 +20,16 @@ class Dataset:
         self.X = X
         self.y = y
         self.name = name
+
+
+def get_X_y(data):
+    vectorizer = CountVectorizer(analyzer="word",
+                                 tokenizer=None,
+                                 preprocessor=None,
+                                 stop_words=None,
+                                 token_pattern=r'\b\w+\b')
+    docs = [x[0] for x in data]
+    vectorizer.fit(docs)
+    integers_from_strings = [[vectorizer.vocabulary_.get(y.lower()) for y in re.sub(r'[.!,;?]', ' ', x).split() if
+                              vectorizer.vocabulary_.get(y.lower()) is not None] for x in docs]
+    return np.array(integers_from_strings), np.array([x[1] for x in data])
