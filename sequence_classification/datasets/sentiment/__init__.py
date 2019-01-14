4e5a5737a3c969c597c6d0c0adf031854dec02b9
berezowskij <jakub.berezowski@snpgroup.com>
2019-01-14 15:09:25 +0100

    Pull request changes - Add multiple datasets to usage_example

--- sequence_classification/datasets/sentiment/__init__.py
+++ sequence_classification/datasets/sentiment/__init__.py
@@ -7,7 +7,7 @@ class CommentSentimentDataset(DatasetLoader):
 
     @staticmethod
     def load_data(name='Comment Sentiment'):
-        data = np.genfromtxt(CommentSentimentDataset.get_dataset_file(['sentiment', 'amazon_cells_labelled.txt']),
+        data = np.genfromtxt(CommentSentimentDataset.get_dataset_file(['sentiment', 'sentiment_cells_labelled.txt']),
                                       delimiter='\t', encoding="utf-8", dtype=None)
         X, y = get_X_y(data)
         return Dataset(X, y, name)
