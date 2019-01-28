e55d9cc86d1e2839d07429a03a023dfed06e4a10
Piotr Miara <piotr@atopik.pl>
2019-01-28 11:41:48 +0100

    Add backslash in token pattern and delete redundant line

--- sequence_classification/transformers/bag_of_words.py
+++ sequence_classification/transformers/bag_of_words.py
@@ -3,12 +3,11 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 
 class BagOfWordsTransformer(SequenceTransformer):
-    def __init__(self, token_pattern=r'\bw+\b', **kwargs):
+    def __init__(self, token_pattern=r'\b\w+\b', **kwargs):
         self.vectorizer = CountVectorizer(token_pattern=token_pattern, **kwargs)
 
     def fit_transform(self, raw_data):
         strings_from_list = [' '.join([str(x) for x in data]) for data in raw_data]
-        self.vectorizer.fit_transform(strings_from_list)
         return self.vectorizer.fit_transform(strings_from_list).toarray()
 
     def transform(self, raw_data):
