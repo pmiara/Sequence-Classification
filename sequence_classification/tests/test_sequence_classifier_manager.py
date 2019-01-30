be2b54e9234f2cd8d7a0de6b84390595f9999b13
Piotr Miara <piotr@atopik.pl>
2019-01-30 12:56:52 +0100

    Rename tests for SequenceClassifierManager

--- sequence_classification/tests/test_sequence_classifier_manager.py
+++ sequence_classification/tests/test_sequence_classifier_manager.py
@@ -7,32 +7,32 @@ from .mock import (MockClassifier, MockReader, MockWriter, PARAMS, DATASET,
 
 def test_should_add_classifier():
     # given
-    seq_class_comparator = SequenceClassifierManager(MockWriter(), MockReader())
+    seq_class_manager = SequenceClassifierManager(MockWriter(), MockReader())
     test_class = MockClassifier()
 
     # when
-    seq_class_comparator.add_classifier(test_class)
+    seq_class_manager.add_classifier(test_class)
 
     # then
-    assert len(seq_class_comparator.classifiers_with_params) == 1
+    assert len(seq_class_manager.classifiers_with_params) == 1
 
 
 def test_should_save_correctly_predicted_data():
     # given
     test_writer = MockWriter()
-    seq_class_comparator = SequenceClassifierManager(test_writer, MockReader())
+    seq_class_manager = SequenceClassifierManager(test_writer, MockReader())
     test_class = MockClassifier()
-    seq_class_comparator.add_classifier(test_class)
+    seq_class_manager.add_classifier(test_class)
     X = [1,2,3,4,5]
     y = [1,2,3,4,5]
-    seq_class_comparator.add_custom_dataset(X, y, DATASET_NAME)
+    seq_class_manager.add_custom_dataset(X, y, DATASET_NAME)
 
     # when
-    seq_class_comparator.fit_predict_all()
+    seq_class_manager.fit_predict_all()
 
     # then
     assert test_writer.results[DATASET] == DATASET_NAME
-    assert test_writer.results[CLASSIFIER] == "TEST"
+    assert test_writer.results[CLASSIFIER] == 'TEST'
     assert test_writer.results[PARAMS] == {}
     assert np.array_equal(test_writer.results[CONF_TRAIN_MAT], np.identity(len(test_writer.results[CONF_TRAIN_MAT])))
     assert np.array_equal(test_writer.results[CONF_TEST_MAT], np.identity(len(test_writer.results[CONF_TEST_MAT])))
@@ -41,15 +41,15 @@ def test_should_save_correctly_predicted_data():
 def test_should_save_wrong_predicted_data():
     #given
     test_writer = MockWriter()
-    seq_class_comparator = SequenceClassifierManager(test_writer, MockReader())
+    seq_class_manager = SequenceClassifierManager(test_writer, MockReader())
     test_class = MockClassifier()
-    seq_class_comparator.add_classifier(test_class)
+    seq_class_manager.add_classifier(test_class)
     X = [1,2,3,4,5]
     y = [2,3,4,5,1]
-    seq_class_comparator.add_custom_dataset(X, y, DATASET_NAME)
+    seq_class_manager.add_custom_dataset(X, y, DATASET_NAME)
 
     # when
-    seq_class_comparator.fit_predict_all()
+    seq_class_manager.fit_predict_all()
 
     # then
     assert np.diag(test_writer.results[CONF_TRAIN_MAT]).sum() == 0
@@ -60,15 +60,15 @@ def test_should_change_name_and_params():
     # given
     test_writer = MockWriter()
     params = {"x":["a"]}
-    seq_class_comparator = SequenceClassifierManager(test_writer, MockReader())
+    seq_class_manager = SequenceClassifierManager(test_writer, MockReader())
     test_class = MockClassifier("NAME")
-    seq_class_comparator.add_classifier(test_class, params=params)
+    seq_class_manager.add_classifier(test_class, params=params)
     X = [1,2,3,4,5]
     y = [1,2,3,4,5]
-    seq_class_comparator.add_custom_dataset(X, y, DATASET_NAME)
+    seq_class_manager.add_custom_dataset(X, y, DATASET_NAME)
 
     # when
-    seq_class_comparator.fit_predict_all()
+    seq_class_manager.fit_predict_all()
 
     # then
     assert test_writer.results[CLASSIFIER] == "NAME"
